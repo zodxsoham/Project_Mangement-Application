2f3e7ba3c3d479b24b02d71e8c98c917ef6e3cc9
SohamBile <sohambile99943@gmail.com>
2026-04-22 00:27:07 +0530

    connect mongodb server

--- src/db/Database.js
+++ src/db/Database.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected");
   } catch (error) {
     console.error("mongoDB connection error", error);
     process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDb from "./db/Database.js";
 
 dotenv.config({
   path: "./.env",
@@ -11,6 +12,13 @@ app.get("/instgram", (req, res) => {
   res.send("Instgram is running");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("mongoDB connection error", error);
+    process.exit(1);
+  });
